260a5803c78129392030e8226a9dbc64c2a26eee
aarnisi <140318765+aarnisi@users.noreply.github.com>
2023-10-25 22:34:00 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='translate',
+    version='1.0.0',
+    url = 'https://github.com/aarnisi/fin-en-fin-data-translation/',
+    packages=find_packages(),
+    install_requires=[
+        'pandas'
+       ,'transformers'
+      ,'pyspark'
+    ],
+)
